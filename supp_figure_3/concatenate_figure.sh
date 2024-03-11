5c5c5839850d9f48984645513a3574f3e4dccd47
adoni5 <roryjmunro1@gmail.com>
2024-03-11 18:02:40 +0000

    PDF versions of figures

--- supp_figure_3/concatenate_figure.sh
+++ supp_figure_3/concatenate_figure.sh
@@ -16,9 +16,9 @@ bottom_height=$((top_height / 4))
 
 
 # Add labels to the bottom images and sizes
-magick Barcode02_chr17_chr15_fusion.ribbon.png -resize ${bottom_width}x${bottom_height}! -gravity NorthWest -font "${FONT}" -pointsize $POINTSIZE -annotate +30+10 'B' Barcode02_chr17_chr15_fusion.ribbon.labelled.png 
+magick Barcode02_chr17_chr15_fusion.ribbon.png -resize ${bottom_width}x${bottom_height}! -gravity NorthWest -font "${FONT}" -pointsize $POINTSIZE -annotate +30+10 'B' Barcode02_chr17_chr15_fusion.ribbon.labelled.png
 magick Barcode06_chr17_chr15_fusion.ribbon.png  -resize ${bottom_width}x${bottom_height}! -gravity NorthWest -font "${FONT}" -pointsize $POINTSIZE -annotate +30+10 'C' Barcode06_chr17_chr15_fusion.ribbon.labelled.png 
 # Append the bottom row together horizontally
 #
 magick \( Barcode02_chr17_chr15_fusion.ribbon.labelled.png \) \( Barcode06_chr17_chr15_fusion.ribbon.labelled.png \) +append bottom_row.png
-magick samplot_output/figure_4_prom_labelled.png bottom_row.png -append  Supplemental_Fig_S3.tiff
+magick -page 836x1048 samplot_output/figure_4_prom_labelled.png bottom_row.png  -append Supplemental_Fig_S3.pdf
